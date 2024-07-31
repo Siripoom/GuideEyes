fb14bec90a750b5f1dda59ca12f3981f9ab197ef
NoT612 <nottymbk@gmail.com>
2024-07-31 23:33:41 +0700

    Create conponents speech TH language

--- App.tsx
+++ App.tsx
@@ -1,6 +1,8 @@
+import React from 'react';
 import {TamaguiProvider, createTamagui} from '@tamagui/core';
 import {Button} from 'tamagui';
 import {config} from '@tamagui/config/v3';
+import {SpeechScreen} from './src/screens';
 // you usually export this from a tamagui.config.ts file
 
 const tamaguiConfig = createTamagui(config);
@@ -15,7 +17,8 @@ export default () => {
   return (
     <TamaguiProvider config={tamaguiConfig}>
       {/* your app here */}
-      <Button theme="blue">Hello world</Button>
+      {/* <Button theme="blue">Hello world</Button> */}
+      <SpeechScreen />
     </TamaguiProvider>
   );
 };

928e1d914531b21ccd0be2092f5d82ffc1b1c917
Siripoom <artorsiriratpoom@gmail.com>
2024-08-07 18:53:49 +0700

    set up and user show maps

--- App.tsx
+++ App.tsx
@@ -1,24 +1,39 @@
 import React from 'react';
-import {TamaguiProvider, createTamagui} from '@tamagui/core';
-import {Button} from 'tamagui';
+import {TamaguiProvider, View, createTamagui} from '@tamagui/core';
 import {config} from '@tamagui/config/v3';
 import {SpeechScreen} from './src/screens';
-// you usually export this from a tamagui.config.ts file
+import MapView from 'react-native-maps';
+import {StyleSheet} from 'react-native';
 
 const tamaguiConfig = createTamagui(config);
-// TypeScript types across all Tamagui APIs
 
 type Conf = typeof tamaguiConfig;
 
 declare module '@tamagui/core' {
   interface TamaguiCustomConfig extends Conf {}
 }
+
 export default () => {
   return (
     <TamaguiProvider config={tamaguiConfig}>
-      {/* your app here */}
-      {/* <Button theme="blue">Hello world</Button> */}
       <SpeechScreen />
+      <View style={{flex: 1}}>
+        <MapView
+          style={styles.map}
+          initialRegion={{
+            latitude: 37.78825,
+            longitude: -122.4324,
+            latitudeDelta: 0.0922,
+            longitudeDelta: 0.0421,
+          }}
+        />
+      </View>
     </TamaguiProvider>
   );
 };
+
+const styles = StyleSheet.create({
+  map: {
+    ...StyleSheet.absoluteFillObject,
+  },
+});

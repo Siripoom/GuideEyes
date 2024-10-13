061d49cb1868fcf3efd75051c9eaaf340f2ffb6e
Siripoom <artorsiriratpoom@gmail.com>
2024-10-14 00:19:26 +0700

    Now can receive GPS location(Emulator)

--- src/screens/MapScreen.tsx
+++ src/screens/MapScreen.tsx
@@ -1,6 +1,7 @@
 import React, {Component} from 'react';
 import {Text, StyleSheet, View} from 'react-native';
 import Maps from '../components/Maps';
+import GoogleMapWebView from '../components/GoogleMapWebView';
 
 export const MapScreen = () => {
   return (

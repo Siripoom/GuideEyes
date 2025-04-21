195e05cc8739b0d4e362fb0c4e2bcfaa4a602f10
Siripoom <artorsiriratpoom@gmail.com>
2025-04-21 11:56:59 +0700

    add plugin

--- babel.config.js
+++ babel.config.js
@@ -1,4 +1,12 @@
 module.exports = {
   presets: ['module:@react-native/babel-preset'],
-  plugins: [['react-native-worklets-core/plugin']],
+  plugins: [
+    [
+      'react-native-worklets-core/plugin',
+      ,
+      {
+        globals: ['__labelImage'],
+      },
+    ],
+  ],
 };
